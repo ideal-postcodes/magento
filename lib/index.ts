06e0239b348be07504831ee95da30339dde0b536
Christopher Blanchard <git@nf3.co>
2020-06-17 18:10:44 +0000

    Bindings spike

---
+++ lib/index.ts
@@ -0,0 +1,5 @@
+import { setup } from "@ideal-postcodes/jsutil";
+
+import { bindings } from "./bindings";
+
+setup({ bindings, window });
---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import ts from "@wessberg/rollup-plugin-ts";
+
+const banner = `/**
+ * @license
+ * Ideal Postcodes <https://ideal-postcodes.co.uk>
+ * Magento Integration
+ * Copyright IDDQD Limited, all rights reserved
+ */`;
+
+// Configure terser to ignore build info banner
+const terserConfig = {
+  output: {
+    comments: (_, { value, type }) => {
+      if (type === "comment2") return /@license/i.test(value);
+    },
+  },
+};
+
+const targets = "ie 11";
+
+export default [
+  {
+    input: "lib/index.ts",
+    output: {
+      file: "./view/base/web/binding.js",
+      banner,
+      format: "umd",
+      name: "IdealPostcodes",
+      exports: "named",
+    },
+    plugins: [
+      resolve({ extensions: [".js", ".ts"] }),
+      commonjs(),
+      ts({
+        transpiler: "babel",
+        browserslist: [targets],
+        babelConfig: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets,
+                useBuiltIns: "usage",
+                corejs: 3,
+              },
+            ],
+          ],
+        },
+      }),
+      terser(terserConfig),
+    ],
+  },
+];

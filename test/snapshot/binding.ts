db330304cecb318a735bc29b1c9a7a35be2e1b91
Christopher Blanchard <git@nf3.co>
2020-06-18 14:06:36 +0200

    Port store bindings to jsutil

--- test/snapshot/binding.ts
+++
@@ -1,5 +0,0 @@
-import { setup } from "@ideal-postcodes/jsutil";
-
-import { bindings } from "../../lib/bindings";
-
-setup({ bindings, window });
--- test/snapshot/rollup.config.js
+++ test/snapshot/rollup.config.js
@@ -1,56 +1,7 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import resolve from "@rollup/plugin-node-resolve";
-import ts from "@wessberg/rollup-plugin-ts";
+import config from "../../rollup.config.js";
 
-const banner = `/**
- * @license
- * Ideal Postcodes <https://ideal-postcodes.co.uk>
- * Magento Integration
- * Copyright IDDQD Limited, all rights reserved
- */`;
+const [bindingConfig] = config;
 
-// Configure terser to ignore build info banner
-const terserConfig = {
-  output: {
-    comments: (_, { value, type }) => {
-      if (type === "comment2") return /@license/i.test(value);
-    }
-  }
-};
+bindingConfig.output.file = "./test/snapshot/fixtures/binding.js";
 
-const targets = "ie 11";
-
-export default [
-  {
-    input: "./test/snapshot/binding.ts",
-    output: {
-      file: "./test/snapshot/fixtures/binding.js",
-      banner,
-      format: "umd",
-      name: "IdealPostcodes",
-      exports: "named"
-    },
-    plugins: [
-      resolve({ extensions: [".js", ".ts"] }),
-      commonjs(),
-      ts({
-        transpiler: "babel",
-        browserslist: [targets],
-        babelConfig: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                targets,
-                useBuiltIns: "usage",
-                corejs: 3
-              }
-            ]
-          ]
-        }
-      }),
-      terser(terserConfig)
-    ]
-  }
-];
+export default [bindingConfig];

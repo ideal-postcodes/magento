40d4648f8f49c9d1f7682822bf2693a67fcc1308
Christopher Blanchard <git@nf3.co>
2020-06-24 22:34:37 +0000

    test(E2E): Add e2e testing

--- test/e2e/cypress/integration/01_checkout.ts
+++ test/e2e/cypress/integration/01_checkout.ts
@@ -6,8 +6,19 @@ Cypress.on("uncaught:exception", (err, runnable) => {
 });
 
 import { address as addresses } from "@ideal-postcodes/api-fixtures";
-const address = addresses.england;
-const secondAddress = addresses.scotland;
+import {
+  setupSuite,
+  autocompleteSuite,
+  postcodeLookupSuite
+} from "../../../snapshot/cypress/support/suite";
+import { selectors } from "../../../../lib/billing";
+
+const address = addresses.jersey;
+const suite = {
+  scope: ".checkout-shipping-address",
+  selectors,
+  address
+};
 
 describe("Checkout", () => {
   before(() => {
@@ -21,66 +32,7 @@ describe("Checkout", () => {
     cy.visit("/index.php/checkout/");
   });
 
-  it("allows postcode lookup and address autocomplete", () => {
-    cy.get("#idpc_input").should("not.exist");
-    cy.get("#idpc_button").should("not.exist");
-
-    // Engage address search
-    cy.get('select[name="country_id"]').select("GB");
-    cy.get("#idpc_input").type(address.postcode);
-    cy.get("#idpc_button").click();
-    cy.get("#idpc_dropdown").select("0");
-    cy.get('input[name="street[0]"]').should("have.value", address.line_1);
-    cy.get('input[name="city"]').should("have.value", address.post_town);
-    cy.get('input[name="postcode"]').should("have.value", address.postcode);
-
-    // Test autocomplete
-    cy.get('input[name="street[0]"]').type(secondAddress.postcode);
-    cy.get(".idpc_ul li")
-      .first()
-      .click();
-    cy.get('input[name="city"]').should("have.value", secondAddress.post_town);
-    cy.get('input[name="postcode"]').should(
-      "have.value",
-      secondAddress.postcode
-    );
-
-    // Complete shipping form and go to billing page
-    cy.get("#customer-email").type("joe@email.com");
-    cy.get('input[name="firstname"]').type("Joe");
-    cy.get('input[name="lastname"]').type("Address");
-    cy.get('input[name="telephone"]').type("07777777777");
-    cy.get(".button")
-      .contains("Next")
-      .click();
-    cy.get(".button")
-      .contains("Next")
-      .click();
-    cy.get("#billing-address-same-as-shipping-checkmo").uncheck();
-
-    // Test billing page
-    cy.get('select[name="country_id"]').select("GB");
-    cy.get("#idpc_input").type(address.postcode);
-    cy.get("#idpc_button").click();
-    cy.get("#idpc_dropdown").select("0");
-    cy.get('input[name="street[0]"]').should("have.value", address.line_1);
-    cy.get('input[name="city"]').should("have.value", address.post_town);
-    cy.get('input[name="postcode"]').should("have.value", address.postcode);
-
-    // Test billing autocomplete
-    cy.get(".billing-address-form").within(() => {
-      cy.get('input[name="street[0]"]').type(secondAddress.postcode);
-      cy.get(".idpc_ul li")
-        .first()
-        .click();
-      cy.get('input[name="city"]').should(
-        "have.value",
-        secondAddress.post_town
-      );
-      cy.get('input[name="postcode"]').should(
-        "have.value",
-        secondAddress.postcode
-      );
-    });
-  });
+  setupSuite(suite, true);
+  postcodeLookupSuite(suite);
+  autocompleteSuite(suite);
 });

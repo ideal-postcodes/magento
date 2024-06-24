4e03722c9075924aa8134a7495726b33da152a88
mfilip <marcin.filip@gmail.com>
2024-06-24 14:29:12 +0200

    fix types

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,4 +1,5 @@
 import { config } from "@ideal-postcodes/jsutil";
+import { Config } from "./extension";
 
 import { bind as orders } from "./admin-orders";
 import { bind as ordersEdit } from "./admin-orders-edit";
@@ -8,5 +9,5 @@ import { bind as custom } from "./admin-custom";
 window.idpcStart = () =>
   [orders, customers, ordersEdit, custom].forEach((bind) => {
     const conf = config();
-    if (conf) bind(conf);
+    if (conf) bind(conf as Config);
   });
